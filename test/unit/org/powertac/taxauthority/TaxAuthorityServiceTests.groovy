dc691c7f63d67b5bf6ddc99fd4dde05b691e9723
ddauer <ddauer@gmail.com>
2011-01-24 17:29:04 +0100

    first commit

---
+++ test/unit/org/powertac/taxauthority/TaxAuthorityServiceTests.groovy
@@ -0,0 +1,17 @@
+package org.powertac.taxauthority
+
+import grails.test.*
+
+class TaxAuthorityServiceTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
